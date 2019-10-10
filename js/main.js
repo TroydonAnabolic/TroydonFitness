63b0be3331938563069683263f1e5cdeed48b24b
Troydon Luicien <tluicien@yahoo.co.nz>
2019-10-10 20:29:38 +1100

    Added search bar and cart

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 // Functions for open nav bar click effect, top open new sub menu below
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("mainNav").style.marginLeft = "250px";
+    $(document).ready(function(){
+        $(".openbtn").click(function(){
+            // Reveal menu when clicked the menu bar
+          $("#reveal-menu").toggleClass("hidden");
+          // hide menu when nav away
+          $("#reveal-menu").mouseleave(function(){
+            $("#reveal-menu").toggleClass("hidden");
+        });
+        });
+      });
 }
 
 // function closeNav() {
 //     document.getElementById("mySidebar").style.width = "0";
 //     document.getElementById("mainNav").style.marginLeft = "0";
 // }
+
+// Animated Hamburger Bar
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+  });

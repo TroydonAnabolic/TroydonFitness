cb485e1f304e1007ed61ec54f1ae65418e5201ef
Troydon Luicien <tluicien@yahoo.co.nz>
2019-10-10 16:18:34 +1100

    was able to add the animated menu bar with bootstrap

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Functions for open nav bar click effect, top open new sub menu below
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mainNav").style.marginLeft = "250px";
+}
+
+// function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("mainNav").style.marginLeft = "0";
+// }

77ae65e0bfa14e331f0bfe126e94d6c216b850ad
Troydon Luicien <tluicien@yahoo.co.nz>
2019-10-11 13:10:17 +1100

    made a few touch ups on login UI

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
 // Toggle search bar
 $(document).ready(function () {
     $(".fa-search").mouseenter(function () {
-        $("input").toggleClass("hidden");
+        $(".search-input").toggleClass("hidden");
     });
-    $("input").mouseleave(function () {
-        $("input").toggleClass("hidden");
+    $(".search-input").mouseleave(function () {
+        $(".search-input").toggleClass("hidden");
     });
 });
 
-// Functions for open nav bar click effect, top open new sub menu below
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    // $(document).ready(function(){
-    //       $(".dropdown-menu").mouseleave(function(){
-    //         $(".dropdown-menu").toggle();
-    //     });
-    //     $("button").mouseenter(function(){
-    //         $(".dropdown-menu").toggle();
-    //     });
-      
-    //   });
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+   if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
      
         
 

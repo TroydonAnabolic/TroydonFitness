ef84a5de0685e58a21dd8f47c7f551e2e60a3a35
Troydon Luicien <tluicien@yahoo.co.nz>
2019-11-18 10:04:31 +1100

    Attempting to display image but encountering error

--- Pages/CustomizedRoutines/Details.cshtml.cs
+++ Pages/CustomizedRoutines/Details.cshtml.cs
@@ -29,7 +29,11 @@ namespace TroydonFitness.Pages.CustomizedRoutines
 
         public List<SelectListItem> Split { get; set; }
 
-
+        public FileResult ServeImage(string path)
+        {
+            var bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, "image/png");
+        }
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)

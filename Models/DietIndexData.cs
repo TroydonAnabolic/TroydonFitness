80f308949b66ce8786e4f24ac216f3065f45a388
Troydon Luicien <troyincarnate@gmail.com>
2019-12-05 09:09:13 +1100

    create diet index data

---
+++ Models/DietIndexData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TroydonFitness.Models.ProductModel;
+
+namespace TroydonFitness.Models
+{
+    public class DietIndexData
+    {
+        public IEnumerable<Diet> Diets { get; set; }
+        public IEnumerable<CustomizedRoutine> CustomizedRoutines { get; set; }
+        public IEnumerable<Supplement> Supplements { get; set; }
+    }
+}

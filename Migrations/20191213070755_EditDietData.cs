ee0ed5f40755d0c10e425ab507c2c71bf1d03fd9
Troydon Luicien <troyincarnate@gmail.com>
2019-12-13 18:08:22 +1100

    Update database to have dietID

---
+++ Migrations/20191213070755_EditDietData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TroydonFitness.Migrations
+{
+    public partial class EditDietData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DietID",
+                table: "Product",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DietID",
+                table: "Product");
+        }
+    }
+}

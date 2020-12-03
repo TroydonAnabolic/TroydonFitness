36b3fcbc1058fc5388fa0f3e1df68ef4185841fb
Troydon Luicien <troyincarnate@gmail.com>
2020-12-03 15:57:50 +1100

    Removed extra files.

--- Create dbo.Product-nocheckFK.sql
+++
@@ -1,8 +0,0 @@
-﻿USE [ProductContext-1dffc610-d94a-47f5-bd24-709854dd78b2]
-GO
-
-
-GO  
-ALTER TABLE [dbo].[Product] 
-NOCHECK CONSTRAINT [FK_Product_PersonalTraining_PersonalTrainingId]
-GO  
--- TroydonFitnessWebsite-master/SQL_Queries/FrequentQueries.sql
+++
@@ -1,6 +0,0 @@
-﻿--Frequently Use Query
--- Database Name: ProductContext_59a25d91_3b23_442e_9df3_9a1daed97a84
--- SET IDENTITY_INSERT to ON.  
---SET IDENTITY_INSERT PersonalTraining ON;  
-SET IDENTITY_INSERT Product ON;  
-  
